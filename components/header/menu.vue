2202f16df9597fef2d19bedee0a5e2e00aec18d5
upplinko <admin@upplinko.com>
2024-11-20 03:48:50 +0530

    latestt

--- components/header/menu.vue
+++ components/header/menu.vue
@@ -29,7 +29,7 @@
                                 <div class="row">
                                     <!-- Column 1: Web Design & Development -->
                                     <ul class="col-md-12 col-lg-3 link-list">
-                                        <li><NuxtLink to="/demo-17">Webbbbb </NuxtLink></li>
+                                        <li><NuxtLink to="/demo-17">WebApps/Websites </NuxtLink></li>
                                         <li><NuxtLink to="/demo-11">Landing Pages</NuxtLink></li>
                                         <li><NuxtLink to="/demo-17">E-commerce Stores</NuxtLink></li>
                                         <li><NuxtLink to="/demo-17">Custom Stack</NuxtLink></li>
