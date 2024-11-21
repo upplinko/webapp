774b9a098a8e63b41378ef2051d92ea9fc3d63da
upplinko <admin@upplinko.com>
2024-11-21 19:06:52 +0530

    Updatedd chatbot sequence

--- components/sections/Demo7/Section11.vue
+++ components/sections/Demo7/Section11.vue
@@ -19,9 +19,19 @@
                         <div class="col">
                             <div class="bc-5-img bc-5-tablet img-block-hidden video-preview wow fadeInUp">
                                 <!-- Play Icon -->
-                                <ElementsCustomModalVideo />
-                                <!-- Preview Image -->
-                                <img class="img-fluid" src="/assets/images/tablet-02.png" alt="content-image" />
+                                <a href="https://www.youtube.com/watch?v=sbVbkS9xP7g" target="_blank">
+                                    <div class="video-thumbnail">
+                                        <img class="img-fluid" src="/assets/images/tablet-02.png" alt="content-image" />
+                                        <div class="play-icon">
+                                            <!-- Play button -->
+                                            <i class="fa fa-play-circle" style="font-size: 50px; color: white; z-index: 10;"></i>
+                                        </div>
+                                    </div>
+                                </a>
+                                <!-- Embed YouTube Video -->
+                                <div class="video-container" style="display:none;">
+                                    <iframe width="100%" height="500" src="https://www.youtube.com/embed/sbVbkS9xP7g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -33,3 +43,43 @@
         <!-- End container -->
     </section>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            isVideoVisible: false
+        };
+    },
+    methods: {
+        toggleVideo() {
+            this.isVideoVisible = !this.isVideoVisible;
+        }
+    }
+};
+</script>
+
+<style scoped>
+.video-preview {
+    position: relative;
+}
+
+.video-thumbnail {
+    position: relative;
+}
+
+.play-icon {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    cursor: pointer;
+    z-index: 100; /* Ensure the play button is above the image */
+}
+
+.video-container iframe {
+    display: block;
+    width: 100%;
+    height: 500px;
+}
+</style>
